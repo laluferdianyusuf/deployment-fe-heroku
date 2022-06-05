80101c610740c01f43c32a0d30d974127d31242f
Ferdian Yusuf <lferdianyusuf@gmail.com>
2022-06-05 19:15:13 +0800

    initial commit

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,16 +1,8 @@
 import React, { useRef, useState } from "react";
-import {
-  Form,
-  Container,
-  Button,
-  Alert,
-  Card,
-  Row,
-  Col,
-} from "react-bootstrap";
+import { Button, Alert } from "react-bootstrap";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
-import regisImage from "../images/airplane.png";
+import "./All.css";
 
 export default function Register() {
   const navigate = useNavigate();
@@ -19,11 +11,6 @@ export default function Register() {
   const emailField = useRef("");
   const roleField = useRef("");
   const passwordField = useRef("");
-
-  const buttonPrimary = {
-    backgroundColor: "rgb(10, 2, 77)",
-    borderRadius: "0px",
-  };
 
   const buttonBorder = {
     border: "1px solid darkblue",
@@ -66,73 +53,53 @@ export default function Register() {
   };
 
   return (
-    <Card className="w-75 text-black" style={{ margin: "1% 11%" }}>
-      <Container className="my-5">
-        <Row>
-          <h1 className="mb-3 text-center">Sign Up</h1>
-          <Col md={5}>
-            <img
-              src={regisImage}
-              className="w-75"
-              style={{ margin: "10% 20% 0 20%" }}
-              alt=""
-            />
-          </Col>
-          <Col md={7} className="w-50 ms-3">
-            <Form onSubmit={onRegister}>
-              <Form.Group className="mb-3">
-                <Form.Label>Role</Form.Label>
-                <Form.Select ref={roleField} style={buttonBorder}>
-                  <option hidden>Choose a role</option>
-                  <option value="admin">Admin</option>
-                  <option value="user">User</option>
-                </Form.Select>
-              </Form.Group>
-              <Form.Group className="mb-3">
-                <Form.Label>Name</Form.Label>
-                <Form.Control
-                  style={buttonBorder}
-                  type="text"
-                  ref={nameField}
-                  placeholder="Name"
-                />
-              </Form.Group>
-              <Form.Group className="mb-3">
-                <Form.Label>Email</Form.Label>
-                <Form.Control
-                  type="text"
-                  style={buttonBorder}
-                  ref={emailField}
-                  placeholder="Email"
-                />
-              </Form.Group>
-              <Form.Group className="mb-3">
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                  style={buttonBorder}
-                  type="password"
-                  ref={passwordField}
-                  placeholder="Password"
-                />
-              </Form.Group>
-              <p>
-                Have an account ?{" "}
-                <Link to="/login" className="text-decoration-none">
-                  Login
-                </Link>
-              </p>
-              {errorResponse.isError && (
-                <Alert variant="danger">{errorResponse.message}</Alert>
-              )}
-              <div className="text-center">
-                <Button style={buttonPrimary} className="w-100" type="submit">
-                  Sign up
-                </Button>
-              </div>
-            </Form>
-          </Col>
-        </Row>
-      </Container>
-    </Card>
+    <div className="register-box">
+      <h2>Sign Up</h2>
+
+      <form onSubmit={onRegister}>
+        <div className="signup-box">
+          <label>Role</label>
+          <select ref={roleField} style={buttonBorder}>
+            <option hidden>Choose a role</option>
+            <option value="admin">Admin</option>
+            <option value="user">User</option>
+          </select>
+        </div>
+
+        <div className="signup-box">
+          <label>Name</label>
+          <input type="text" ref={nameField} />
+        </div>
+
+        <div className="signup-box">
+          <label>Email</label>
+          <input type="text" ref={emailField} />
+        </div>
+
+        <div className="signup-box">
+          <label>Password</label>
+          <input type="password" ref={passwordField} />
+        </div>
+
+        <p>
+          Have an account ?{" "}
+          <Link to="/login" className="text-decoration-none">
+            Login
+          </Link>
+        </p>
+        {errorResponse.isError && (
+          <Alert variant="danger">{errorResponse.message}</Alert>
+        )}
+        <div className="text-center">
+          <Button className="button-submit" type="submit">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            Sign up
+          </Button>
+        </div>
+      </form>
+    </div>
   );
 }
